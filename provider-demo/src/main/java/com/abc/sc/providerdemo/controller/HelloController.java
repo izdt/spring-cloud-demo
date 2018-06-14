25cf0520f8462655a961b85d69bbc2cf6d924636
iZDT <dev@dthappy.com>
2018-06-14 18:01:40 +0800

    add error configuration to provider

--- provider-demo/src/main/java/com/abc/sc/providerdemo/controller/HelloController.java
+++ provider-demo/src/main/java/com/abc/sc/providerdemo/controller/HelloController.java
@@ -12,8 +12,11 @@ class HelloController{
     private String port;
     @Value("${app.config.delay}")
     private int delay;
+    @Value("${app.config.error}")
+    private boolean isError;
     @RequestMapping("/hello")
     public String helloWorld(@RequestParam String name) throws Exception{
+        if(isError) throw new Exception("Custom Error");
         Thread.sleep(delay);
         return "hello " + name + ", from " + port;
     }
