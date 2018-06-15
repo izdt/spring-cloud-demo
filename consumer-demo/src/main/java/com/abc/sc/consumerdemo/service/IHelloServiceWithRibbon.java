d01581ab69fa08f55530308f2e1d5a144f225d2d
iZDT <dev@dthappy.com>
2018-06-15 16:02:05 +0800

    Add hystrix to ribbon

--- consumer-demo/src/main/java/com/abc/sc/consumerdemo/service/IHelloServiceWithRibbon.java
+++ consumer-demo/src/main/java/com/abc/sc/consumerdemo/service/IHelloServiceWithRibbon.java
@@ -1,5 +1,6 @@
 package com.abc.sc.consumerdemo.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,7 +11,11 @@ public class IHelloServiceWithRibbon{
 
     @Autowired
     RestTemplate restTemplate;
+    @HystrixCommand(fallbackMethod = "helloMethodFallback")
     public String testHelloMethod(String name) {
         return restTemplate.getForObject("http://spring-cloud-provider-demo/demo/hello?name="+name,String.class);
+    }
+     public String helloMethodFallback(String name) {
+        return "hello service from ribbon not available";
     }
 }
