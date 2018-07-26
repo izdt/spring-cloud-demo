f73cbb405205b62dc290318ac2d108ffbb832bd0
ZDT@VAIO <dev@dthappy.com>
2018-07-26 18:37:15 +0800

    Change body filter

--- zuul-demo/src/main/java/com/abc/sc/zuuldemo/filter/DemoFilter.java
+++ zuul-demo/src/main/java/com/abc/sc/zuuldemo/filter/DemoFilter.java
@@ -1,21 +1,34 @@
 package com.abc.sc.zuuldemo.filter;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
 import javax.servlet.http.HttpServletRequest;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
-import org.springframework.stereotype.Component;
+
+import org.springframework.util.StreamUtils;
 
 public class DemoFilter extends ZuulFilter{
 
 	@Override
 	public Object run() throws ZuulException {
-		RequestContext ctx = RequestContext.getCurrentContext();
+        RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
-        
-
-        
+        try {
+            InputStream in = (InputStream) ctx.get("requestEntity");
+            if (in == null) {
+                in = request.getInputStream();
+            }
+            String body = StreamUtils.copyToString(in, Charset.forName("UTF-8"));
+            body = body.toUpperCase();//TODO
+            ctx.set("requestEntity", new ByteArrayInputStream(body.getBytes("UTF-8")));
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
         System.out.println(String.format("%s DemoFilter request to %s", request.getMethod(), request.getRequestURL().toString()));        
         return request;
 	}
