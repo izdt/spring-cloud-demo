cddff423bb297599a40ee0e55ebc386b5b536b82
ZDT@VAIO <dev@dthappy.com>
2018-07-26 18:12:22 +0800

    change to enable discovery client

--- consumer-demo/src/main/java/com/abc/sc/consumerdemo/ConsumerDemoApplication.java
+++ consumer-demo/src/main/java/com/abc/sc/consumerdemo/ConsumerDemoApplication.java
@@ -3,13 +3,14 @@ package com.abc.sc.consumerdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @EnableHystrix
 @EnableCircuitBreaker
