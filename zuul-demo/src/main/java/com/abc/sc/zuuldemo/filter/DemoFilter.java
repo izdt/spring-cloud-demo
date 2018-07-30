2322a038794bcc518e6e3fcaa94dda9d544f759f
ZDT@VAIO <dev@dthappy.com>
2018-07-30 11:30:08 +0800

    update filter to get request body

--- zuul-demo/src/main/java/com/abc/sc/zuuldemo/filter/DemoFilter.java
+++ zuul-demo/src/main/java/com/abc/sc/zuuldemo/filter/DemoFilter.java
@@ -1,41 +1,42 @@
 package com.abc.sc.zuuldemo.filter;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
-
 import javax.servlet.http.HttpServletRequest;
-
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import com.netflix.zuul.exception.ZuulException;
 
 import org.springframework.util.StreamUtils;
+import static com.netflix.zuul.context.RequestContext.getCurrentContext;
 
 public class DemoFilter extends ZuulFilter{
 
 	@Override
-	public Object run() throws ZuulException {
-        RequestContext ctx = RequestContext.getCurrentContext();
-        HttpServletRequest request = ctx.getRequest();
-        try {
-            InputStream in = (InputStream) ctx.get("requestEntity");
-            if (in == null) {
-                in = request.getInputStream();
-            }
-            String body = StreamUtils.copyToString(in, Charset.forName("UTF-8"));
-            body = body.toUpperCase();//TODO
-            ctx.set("requestEntity", new ByteArrayInputStream(body.getBytes("UTF-8")));
-        } catch (Exception e) {
-            //TODO: handle exception
-        }
-        System.out.println(String.format("%s DemoFilter request to %s", request.getMethod(), request.getRequestURL().toString()));        
-        return request;
-	}
-
-	@Override
 	public boolean shouldFilter() {
 		return true;
+	}
+	@Override
+	public Object run() {
+		try {
+			RequestContext context = getCurrentContext();
+            HttpServletRequest request=(HttpServletRequest)context.getRequest();
+			InputStream is=request.getInputStream();
+			/*
+			InputStream in = (InputStream) context.get("requestEntity");
+			if (in == null) {
+				in = context.getRequest().getInputStream();
+			}
+			*/
+			String body = StreamUtils.copyToString(is, Charset.forName("UTF-8"));
+			body = body.toUpperCase();
+			context.set("requestEntity", new ByteArrayInputStream(body.getBytes("UTF-8")));
+			System.out.println("Request content:"+body);
+        } catch (IOException e) {
+			e.printStackTrace();
+        }
+		return null;
 	}
 
 	@Override
