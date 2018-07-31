e5b75245352ba087bf3e723b0094851b2088d09f
ZDT@VAIO <dev@dthappy.com>
2018-07-31 17:45:52 +0800

    update for swagger

--- provider-demo/src/main/java/com/abc/sc/providerdemo/controller/HelloController.java
+++ provider-demo/src/main/java/com/abc/sc/providerdemo/controller/HelloController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(description = "测试接口")
 class DemoController{
     @Value("${server.port}")
     private String port;
     @Value("${app.config.delay}")
     private int delay;
+
+    @ApiOperation(value = "TEST01 查询接口", notes = "获取所有用户信息")
     @PostMapping("/hello")
     public DemoResponse helloWorld(@RequestBody DemoRequest request) throws Exception{
         Thread.sleep(delay);
