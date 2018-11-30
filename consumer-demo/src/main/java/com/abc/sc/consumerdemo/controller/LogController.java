772ff46c90c3972dcd07e09b1f9bc5ca1d052e2f
ZDT@VAIO <dev@dthappy.com>
2018-11-30 15:10:18 +0800

    Add demo for micrometer and atlas

--- consumer-demo/src/main/java/com/abc/sc/consumerdemo/controller/LogController.java
+++ consumer-demo/src/main/java/com/abc/sc/consumerdemo/controller/LogController.java
@@ -9,17 +9,23 @@ import com.abc.sc.consumerdemo.service.IMiddleService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.MetricsEndpoint;
+import org.springframework.boot.actuate.metrics.MetricsEndpoint.MetricResponse;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import brave.Span;
 import brave.Tracer;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 
 
 @RestController
 public class LogController{
     private static final Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
     @Autowired
     private Tracer tracer;
     @Autowired
@@ -28,6 +34,19 @@ public class LogController{
     private IHelloServiceWithRibbon ribbonService;
     @Autowired
     private IMiddleService middleService;
+    @Autowired
+    private MetricsEndpoint metricsEndpoint;
+    @Autowired
+    private MeterRegistry metricRegistry;    
+  
+    @Bean
+    public Timer getTimer(){
+        //Will add tag to all metrics
+        metricRegistry.config().commonTags("serviceid","consumer-service","appid","SDC20170301");
+        return metricRegistry.timer("acm.requests", "trcode","TESTO001");
+    }
+    @Autowired
+    private Timer timer;  
 
     @RequestMapping("/log/hello/{name}")
     public String sayHello(@PathVariable(value="name") String name){
@@ -47,5 +66,21 @@ public class LogController{
     public String showTrace(@PathVariable(value="name") String name){
         log.info("Hello from spring cloud consumer showTrace with "+name);
         return middleService.testMiddleMethod(name); 
+    }
+    @RequestMapping("/log/metrics/{name}")
+    public MetricResponse showMetrics(@PathVariable(value="name") String name){
+        Long random = (long) (Math.random() * 100);
+        Timer.Sample sample = Timer.start(metricRegistry);
+        try {
+            Thread.sleep(random);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        sample.stop(timer);
+
+        // Counter conunter = metricRegistry.find(name).counter();
+        // metricRegistry.counter(name, tags);
+
+        return metricsEndpoint.metric(name, null);
     }
 }
---
+++ elk-kafka-demo/atlas.md
@@ -0,0 +1,8 @@
+# Atlas demo
+
+document at : https://github.com/Netflix/atlas/wiki/Getting-Started
+
+```
+$ curl -LO https://github.com/Netflix/atlas/releases/download/v1.5.3/atlas-1.5.3-standalone.jar
+$ java -jar atlas-1.5.3-standalone.jar
+```
