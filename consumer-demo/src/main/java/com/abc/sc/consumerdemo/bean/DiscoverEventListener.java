ad57a66017bf791b90c502baeffefa00fcd18f4b
ZDT@VAIO <dev@dthappy.com>
2019-01-07 11:36:16 +0800

    update discover config to add http basic and register eureka envent

--- consumer-demo/src/main/java/com/abc/sc/consumerdemo/bean/DiscoverEventListener.java
+++ consumer-demo/src/main/java/com/abc/sc/consumerdemo/bean/DiscoverEventListener.java
@@ -2,7 +2,6 @@ package com.abc.sc.consumerdemo.bean;
 
 import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.netflix.discovery.CacheRefreshedEvent;
@@ -11,7 +10,6 @@ import com.netflix.discovery.EurekaEvent;
 import com.netflix.discovery.EurekaEventListener;
 import com.netflix.discovery.StatusChangeEvent;
 import com.netflix.discovery.shared.Applications;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,21 +21,19 @@ public class DiscoverEventListener implements EurekaEventListener {
     private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     //protected final Log logger = LogFactory.getLog(getClass());
     @Autowired
-    // private EurekaClient eurekaClient;
     private DiscoveryClient discoveryClient;
     @Autowired
-    // private EurekaClient eurekaClient;
     private EurekaClient eurekaClient;
 
     private Applications currentApps;
     private String currentAppHashCode="N/A";
     @Override
     public void onEvent(EurekaEvent event) {
-        logger.info(event.toString());
         if(event instanceof CacheRefreshedEvent){
             Applications apps = eurekaClient.getApplications();
-            if(!currentAppHashCode.equals(apps.getAppsHashCode())){
-                logger.info("Eureka Applications is changed, current hashcode {}, previous hashcode {}",apps.getAppsHashCode(),currentAppHashCode);
+            String hashcode = apps.getReconcileHashCode();
+            if(!currentAppHashCode.equals(hashcode)){
+                logger.info("Eureka Applications has been changed, current hashcode {}, previous hashcode {}",hashcode,currentAppHashCode);
                 if(logger.isDebugEnabled()){
                     ObjectMapper mapper = new ObjectMapper();
                     try {
@@ -48,10 +44,10 @@ public class DiscoverEventListener implements EurekaEventListener {
                         logger.error("Format eureka applications failed, exception: {}", e.getMessage());
                     }
                 }
-                currentAppHashCode = apps.getAppsHashCode();
+                currentAppHashCode = hashcode;
                 currentApps = apps;
             }
-            logger.info(Arrays.toString(discoveryClient.getServices().toArray()));
+            logger.info("{}, services: {}",event.toString(),Arrays.toString(discoveryClient.getServices().toArray()));
         }
         if(event instanceof StatusChangeEvent){
             //StatusChangeEvent scEvent = (StatusChangeEvent)event;
--- consumer-demo/src/main/java/com/abc/sc/consumerdemo/config/DiscoverConfiguration.java
+++ consumer-demo/src/main/java/com/abc/sc/consumerdemo/config/DiscoverConfiguration.java
@@ -1,18 +1,91 @@
 package com.abc.sc.consumerdemo.config;
 
-import com.abc.sc.consumerdemo.bean.DiscoverEventListener;
-import com.netflix.discovery.EurekaClient;
+import java.util.*;
 
+import com.abc.sc.consumerdemo.bean.DiscoverEventListener;
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.discovery.DefaultEurekaClientConfig;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.EurekaClientConfig;
+
+import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl;
+import com.sun.jersey.api.client.filter.ClientFilter;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.SearchStrategy;
+import org.springframework.cloud.netflix.eureka.EurekaClientAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+import javax.annotation.PostConstruct;
+
 @Configuration
+//@AutoConfigureAfter(EurekaClientAutoConfiguration.class)
 public class DiscoverConfiguration{
     @Autowired
-    private DiscoverEventListener eventListener;
-
+    private EurekaClient discoveryClient;
     @Autowired
+    private DiscoverEventListener eventListener;
+    @Autowired
+    private ApplicationInfoManager appInfoManager;
+    @Autowired
+    private EurekaClientConfigBean eurekaClientConfigBean;
+    @Autowired
+    EurekaClientConfig eurekaConfig;
+
+
+    @PostConstruct
     public void run(){
         discoveryClient.registerEventListener(eventListener);
+        appInfoManager.getInfo().getMetadata().put("appid-new","ABC");
+
+        Map<String, String> serviceUrls = new HashMap<>();
+        //serviceUrls.put("defaultZone", "http://abc:abcabc@localhost:10101/eureka/");
+
+        eurekaClientConfigBean.setServiceUrl(serviceUrls);
+    }
+    @Bean
+    @ConditionalOnMissingBean(DiscoveryClient.DiscoveryClientOptionalArgs.class)
+    public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs(){
+
+        DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
+        /*
+        EurekaJerseyClientImpl.EurekaJerseyClientBuilder builder = new EurekaJerseyClientImpl.EurekaJerseyClientBuilder();
+        args.setEurekaJerseyClient(builder.build());
+        */
+        List<ClientFilter> filters = new ArrayList<>();
+        filters.add(new HTTPBasicAuthFilter("abc", "abcabc"));
+        args.setAdditionalFilters(filters);
+        return args;
+    }
+    //@Bean
+    public EurekaClientConfig eurekaClientConfigBean() {
+        //EurekaClientConfigBean eurekaClientConfigBean = new EurekaClientConfigBean();
+
+        Map<String, String> serviceUrls = new HashMap<>();
+        serviceUrls.put("defaultZone", "http://127.0.0.1:8762/eureka/");
+        eurekaClientConfigBean.setServiceUrl(serviceUrls);
+
+
+
+        /*
+        Map<String, String> env = System.getenv();
+
+        if (env.get("REGISTRY_SERVICE_HOST") != null) {
+
+            String host = env.get("REGISTRY_SERVICE_HOST");
+            Map<String, String> serviceUrls = new HashMap<>();
+            serviceUrls.put("defaultZone", "http://"+host+":8761/eureka/");
+            eurekaClientConfigBean.setServiceUrl(serviceUrls);
+
+        }
+        */
+        return eurekaClientConfigBean;
     }
 }
